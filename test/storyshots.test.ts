4aee93c7d9c59e03147d6fa3f195491ac56d7cd3
Mathieu Guyot <mathieuguyot40@gmail.com>
2020-10-21 16:22:14 +0200

    Updated react version

--- test/storyshots.test.ts
+++ test/storyshots.test.ts
@@ -1,5 +1,8 @@
-import initStoryshots from "@storybook/addon-storyshots";
+import path from 'path';
+import initStoryshots, { multiSnapshotWithOptions } from "@storybook/addon-storyshots";
  
 initStoryshots({
-    storyKindRegex: "Test/.*"
+    integrityOptions: { cwd: path.resolve(__dirname, '../src/stories') },
+    configPath: path.resolve(__dirname, '../.storybook'),
+    test: multiSnapshotWithOptions()
 });
